8f77b5b48782a6a2b2710ae7b699272a98afb20d
koshigoe <koshigoeb@gmail.com>
2018-11-14 13:34:14 +0900

    chore: update benchmark.

--- examples/use-instead-of-ruby-csv/benchmark-parse.rb
+++ examples/use-instead-of-ruby-csv/benchmark-parse.rb
@@ -11,8 +11,18 @@ Benchmark.bm(20) do |x|
     end
   end
 
-  x.report('csv-msgpack encode') do
+  x.report('csv-msgpack - Golang') do
     Open3.popen3("../../csv-msgpack encode -i #{path}") do |stdin, stdout, stderr, wait_thr|
+      stdin.close
+
+      u = MessagePack::Unpacker.new(stdout)
+      u.each do |_|
+      end
+    end
+  end
+
+  x.report('csv-msgpack - Rust') do
+    Open3.popen3("../../target/release/csv-msgpack < #{path}") do |stdin, stdout, stderr, wait_thr|
       stdin.close
 
       u = MessagePack::Unpacker.new(stdout)
@@ -28,3 +38,8 @@ ruby 2.5.3p105 (2018-10-18 revision 65156) [x86_64-darwin18]
                            user     system      total        real
 CSV.foreach          251.196207   1.892531 253.088738 (273.155054)
 csv-msgpack encode     0.761726   0.084683   5.311952 (  4.500843)
+
+                           user     system      total        real
+CSV.foreach          225.262108   1.418877 226.680985 (238.651944)
+csv-msgpack - Golang   0.565393   0.093311   4.628336 (  3.944491)
+csv-msgpack - Rust     0.544291   0.075604   1.544383 (  0.965783)
