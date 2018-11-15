d648d936ef004a860563195b1e1b26a43c9cd2dc
koshigoe <koshigoeb@gmail.com>
2018-11-15 19:26:15 +0900

    test: refine benchmark script.

---
+++ benchmark/benchmark-parse.rb
@@ -0,0 +1,23 @@
+require 'benchmark'
+require 'csv'
+require 'msgpack'
+require 'open3'
+
+path = ARGV[0]
+
+Benchmark.bm(10) do |x|
+  x.report('csv-msgpack') do
+    Open3.popen3("../target/release/csv-msgpack < #{path}") do |stdin, stdout, stderr, wait_thr|
+      stdin.close
+
+      u = MessagePack::Unpacker.new(stdout)
+      u.each do |_|
+      end
+    end
+  end
+
+  x.report('CSV.foreach') do
+    CSV.foreach(path) do |_|
+    end
+  end
+end
