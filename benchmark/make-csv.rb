195bf498f7f965cdf500f33879ebedce1aaa20ee
koshigoe <koshigoeb@gmail.com>
2018-11-15 21:16:59 +0900

    test: refine benchmark script.

--- benchmark/make-csv.rb
+++ benchmark/make-csv.rb
@@ -1,18 +1,16 @@
 require 'csv'
+require 'securerandom'
 
 header = Array.new(99) { |i| i }
-cols = header.map do |i|
-  chr = ('A'.ord + (i % 26)).chr
-  Array.new(100, chr).join("\n")
-end
 
 L = 10_000
 
-File.open("100chr-100col-#{L}row.csv", 'wb') do |f|
+File.open('data.csv', 'wb') do |f|
   f.puts(CSV.generate_line((['id'] + header)))
 
   L.times do |i|
-    r = [format('%0100d', i)] + cols
+    r = [format('%020d', i)]
+    r += Array.new(99) { SecureRandom.hex(10).chars.join("\n") }
     f.puts(CSV.generate_line(r))
   end
 end
